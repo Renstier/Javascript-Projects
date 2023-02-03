e632cac695560aeecaa8cc90206d6251c46414a0
Renstier <stephenwaitejr@gmail.com>
2023-02-03 15:18:49 -0700

    updated file

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,4 +1,7 @@
-var B = "Deez";
-window.alert(B);
-var A = "This is a string";
-document.write(A);
+var B = "Deez";  // Defining a variable
+window.alert(B); // displaying a variable
+                 // a string value
+var Family = "the Arezzinis", Dad = "Jeremiah", Mom = "Hermione",  // Defining Multiple variables 
+Daughter = "Penny", Son = "Zorro";
+document.write(Son); // Displaying a single variable 
+document.write(3 + 66); // defining and displaying a expression
