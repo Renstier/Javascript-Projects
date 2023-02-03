8808eebed340282adc7cc9a35cca4173cf63a9ad
Renstier <stephenwaitejr@gmail.com>
2023-02-03 15:52:08 -0700

    updated 2 files

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() { // Th function at wil use the += operator
+    var sentence = "I am learning"; // creating the variable
+    sentence += " a lot from this book!"; // using the += operator to add to the variable
+    document.getElementById('concatenate').innerHTML = sentence; // using documant.getElementById()
+}
