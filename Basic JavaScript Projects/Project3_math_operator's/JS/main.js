c500a15594eda201940074af36b790703ada221a
Renstier <stephenwaitejr@gmail.com>
2023-02-07 13:51:52 -0700

    updated files

--- Basic JavaScript Projects/Project3_math_operator's/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operator's/JS/main.js
@@ -1,6 +1,6 @@
-function addition_Function() {
-    var addition = 2 + 2;
-    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 -5;
+    document.getElementById("Math").innerHTML = "1 plus 2, multiplied  by 10, divided in half and then subtracted by 5 equals " + simple_Math;
 }
 
 window.alert(Math.random() * 100);
